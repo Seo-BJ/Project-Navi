58d6a5b98238db5833fc0e22f1963b11ed4f255b
서범준 / BeomJun Seo <88536510+Seo-BJ@users.noreply.github.com>
2026-01-27 20:59:10 +0900

    Chore: 사용하지 않는 클래스 삭제

--- Plugins/GameFeatures/NaviShooterCore/Source/NaviShooterCoreRuntime/Private/Inventory/NaviInventoryFragment_AltFire.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Inventory/NaviInventoryFragment_AltFire.h"
-
-void UNaviInventoryFragment_AltFire::GetAbilitySetsToGrant(TArray<const ULyraAbilitySet*>& OutAbilitySets) const
-{
-	OutAbilitySets.Append(AbilitySetsToGrant);
-}
